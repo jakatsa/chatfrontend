d7fcf1f6ef037bf1f2de066f3fba0b5b580d880e
jakatsa <jakatsa5@gmail.com>
2025-06-29 15:07:22 +0300

    homepage.jsx and stylings created

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Router>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        {/* <Route path="/login" element={<ProtectedLogin />}/>
+        <Route path="/register" element={<ProtectedRegister />}/>
+        <Route path="/chats" element={
+        <ProtectedRoute>
+            <ChatList />
+        </ProtectedRoute>
+        } />
+        <Route path="/chat/:conversationId" element={<Conversation />} /> */}
+      </Routes>
+    </Router>
+  )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import '../styles/Home.css';
+
+const Home = () => {
+  const users = ["John Doe", "Jane Smith", "Alice Johnson", "Bob Brown"];
+
+  return (
+    <div className="home-container">
+      <header className="home-header">
+        <h1>Welcome to The Chat House</h1>
+        <p>Connect with your people!</p>
+      </header>
+
+    
+    </div>
+  );
+};
+
+export default Home;
